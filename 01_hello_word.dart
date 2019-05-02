7120d93486bba8ed583b07dd1b98dfdf7db5ba2c
xiaochuan1719 <xiaochuan1719@outlook.com>
2019-05-03 00:40:30 +0800

    update hello_word.dart file

---
+++ 01_hello_word.dart
@@ -0,0 +1,13 @@
+void main() {
+
+  // this is my first line of code
+  print('Hello Dart');
+
+  print('This is my first dart app');
+
+  // Performing arithematic operation
+  print( 12/ 4 );
+
+  // Printing out boolean value
+  print(false);
+}
